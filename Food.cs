a120d4e84f31aac5481898cee34e69656fcc502d
Daniel Soots <danielsoots@icloud.com>
2022-01-26 11:00:31 -0500

    created IConsumable, Drink.  Refactored Food to implement IConsumable, fulfilled contract on Drink class with interface.

--- Food.cs
+++ Food.cs
@@ -1,11 +1,15 @@
 namespace HungryNinja
 {
-    class Food
+    class Food : IConsumable
     {
-        public string Name;
-        public int Calories;
-        public bool IsSpicy;
-        public bool IsSweet;
+        public string Name {get;set;}
+        public int Calories {get;set;}
+        public bool IsSpicy {get;set;}
+        public bool IsSweet {get;set;}
+
+        public string GetInfo(){
+            return $"{Name} (Food). Calories: {Calories}. Spicy?: {IsSpicy}, Sweet?: {IsSweet}";
+        }
 
         public Food(string name, int cal, bool isSpicy, bool isSweet){
             Name = name;
--- Program.cs
+++ Program.cs
@@ -11,27 +11,24 @@ namespace HungryNinja
 
             Buffet eats = new Buffet();
             Ninja jay = new Ninja();
-
-
-
             
-            // System.Console.WriteLine(eats.Menu);
-            // foreach (var item in eats.Menu)
-            // {
-            //     System.Console.WriteLine(item.Name);
-            // }
+            // // System.Console.WriteLine(eats.Menu);
+            // // foreach (var item in eats.Menu)
+            // // {
+            // //     System.Console.WriteLine(item.Name);
+            // // }
 
-            // Food numnum = eats.Serve();
-            // System.Console.WriteLine(numnum.Name);
+            // // Food numnum = eats.Serve();
+            // // System.Console.WriteLine(numnum.Name);
 
-            // Food sumNewFood = new Food("Thai", 300, true, false);
-            // jay.Eat(eats.Serve());
-            // System.Console.WriteLine(jay.FoodHistory);
-            // System.Console.WriteLine("==========");
-            // foreach (var item in jay.FoodHistory)
-            // {
-            //     System.Console.WriteLine(item.Name);
-            // }
+            // // Food sumNewFood = new Food("Thai", 300, true, false);
+            // // jay.Eat(eats.Serve());
+            // // System.Console.WriteLine(jay.FoodHistory);
+            // // System.Console.WriteLine("==========");
+            // // foreach (var item in jay.FoodHistory)
+            // // {
+            // //     System.Console.WriteLine(item.Name);
+            // // }
 
             while(!jay.IsFull){
                 jay.Eat(eats.Serve());
